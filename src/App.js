4f6f099ba473cd4a082925cf3ec77c70501604fe
tutq8118 <tutq8118@gmail.com>
2020-07-07 11:45:08 +0700

    connect API: Books list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bootstrap/scss/bootstrap.scss';
+import './App.scss';
+
+import BookList from './components/BookList';
+
+const axios = require('axios').default;
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      books: []
+    }
+  }
+  async componentDidMount() {
+    var booksAPI = await axios
+      .get('http://localhost:8000/api/books')
+    this.setState({
+      books: booksAPI.data
+    });
+  }
+  render() {
+    const {books} = this.state;
+    return <div className="App">
+      <div className="container">
+        <h2 className="mb-3">List Books</h2>
+        <div className="book-cards mb-5">
+          <div className="row">
+            {books.length > 0 && books.map((item, index) => {
+              return <BookList key={index} item={item} />
+            })}
+        </div>
+        </div>
+      </div>
+    </div>;
+  }
 }
 
 export default App;
---
+++ src/components/BookList.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <div className="col-lg-3 col-md-4 col-sm-6 mb-4">
+          <div className="card">
+            <div className="card-body">
+              <figure className="card-img mb-3">
+                <img className="card-img-bottom" src={this.props.item.coverUrl} alt={this.props.item.title} />
+                <h5 className="card-title">{this.props.item.title}</h5>
+                <p className="card-text">{this.props.item.desc}</p>
+                <div className="cart-button text-center">
+                  <button className="btn btn-primary">Add to cart</button>
+                </div>
+              </figure>
+            </div>
+          </div>
+        </div>
+    );
+  }
+}
+
+export default BookList;
+
+// 2) Viết lại page hiển thị các đầu sách bạn đã làm ở chuỗi Express (đặt tên component là BookList)
+// 3) Dữ liệu được tải về từ API endpoints /api/books ở bên glitch của code server
+// 4) Thêm chức năng tìm kiếm sách theo tên (viết cả search endpoint mới ở phía server)
