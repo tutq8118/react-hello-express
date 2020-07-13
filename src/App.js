77eb072ea18744a848fd0b50452cac9a346c9066
tutq8118 <tutq8118@gmail.com>
2020-07-13 13:40:46 +0700

    hook convert

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import 'bootstrap/scss/bootstrap.scss';
 import 'antd/dist/antd.css';
 import './App.scss';
@@ -9,117 +9,122 @@ import LoginForm from './components/LoginForm';
 import MiniCart from './components/MiniCart';
 
 import { Card, Row, Col, Alert, Typography } from 'antd';
-import {
-  BrowserRouter as Router,
-  Route,
-  NavLink
-} from "react-router-dom";
-
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 const { Meta } = Card;
 const { Title } = Typography;
 
 const axios = require('axios').default;
 const REACT_APP_API_SERVER = process.env.REACT_APP_API_SERVER;
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.inputElement = React.createRef();
-    this.buttonElement = React.createRef();
+function App(props) {
+  const inputSearchRef = useRef(null);
+  const buttonElement = useRef(null);
+  const formSearchRef = useRef(null);
 
-    this.state = {
-      books: [],
-    };
-    console.log('contructor');
-  }
-  componentDidMount() {
-    // var booksAPI = await axios.get(`${REACT_APP_API_SERVER}api/books`);
-    axios.get(`${REACT_APP_API_SERVER}api/books`).then(booksAPI => {
-      this.setState({
-        books: booksAPI.data,
-        allBooks: booksAPI.data,
-      });  
-    }).catch(error => console.log("fail to fetch data", error));
+  const [books, setBooks] = useState([]);
+  const [allBooks, setAllBooks] = useState([]);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggle = () => setDropdownOpen((prevState) => !prevState);
+
+  useEffect(() => {
+    axios
+      .get(`${REACT_APP_API_SERVER}api/books`)
+      .then((booksAPI) => {
+        setBooks(booksAPI.data);
+        setAllBooks(booksAPI.data);
+      })
+      .catch((error) => console.log('fail to fetch data', error));
     console.log('component did mount');
-    
-  }
+  }, []);
 
-  handleSearch = () => {
-    const q = this.inputElement.current.value;
-    const { allBooks } = this.state;
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const q = inputSearchRef.current.value;
     if (!allBooks) {
       return;
     }
     const filteredBooks = allBooks.filter((item) => {
       return item.title.toLowerCase().includes(q.toLowerCase()) === true;
     });
-    this.setState({
-      books: filteredBooks,
-    });
+    setBooks(filteredBooks);
   };
 
-  render() {
-    console.log('render');
-    const { books } = this.state;
-    return (
-      <Router >
-        <div className="App">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3">
-            <div className="container">
-              <h1 className="h2">
-                <NavLink className="navbar-brand" to="/" exact>HelloExpress</NavLink>
-              </h1>
-              <div className="collapse navbar-collapse justify-content-between">
-                <ul className="navbar-nav flex-grow-1">
-                  <li className="nav-item">
-                      <NavLink className="nav-link" to="/books" exact>Books</NavLink>
-                    </li>
-                  </ul>
-                <SearchBox>
+  return (
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3">
+          <div className="container">
+            <h1 className="h2">
+              <NavLink className="navbar-brand" to="/" exact>
+                HelloExpress
+              </NavLink>
+            </h1>
+            <div className="collapse navbar-collapse justify-content-between">
+              <ul className="navbar-nav flex-grow-1">
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/books" exact>
+                    Books
+                  </NavLink>
+                </li>
+              </ul>
+              <SearchBox>
+                <form ref={formSearchRef} onSubmit={handleSearch}>
                   <div className="form-group">
-                    <input ref={this.inputElement} className="form-control" type="text" required name="q" placeholder="Type a book name..." onKeyPress={this.handleSearch} />
+                    <input ref={inputSearchRef} className="form-control" type="text" required name="q" placeholder="Type a book name..." />
                   </div>
-                  <button ref={this.buttonElement} className="btn btn-primary" onClick={this.handleSearch}>
-                    Search
-                  </button>
-                </SearchBox>
-                <div className="navbar__login ml-lg-3">
-                  <NavLink className="navbar__login-link" to="/login" exact>Login</NavLink>
-                </div>
-                <MiniCart />
+                  <button className="btn btn-primary">Search</button>
+                </form>
+              </SearchBox>
+              <div className="navbar__login ml-lg-3">
+                <NavLink className="navbar__login-link" to="/login" exact>
+                  Login
+                </NavLink>
+              </div>
+              <div className="navbar__cart ml-lg-3">
+                <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+                  <DropdownToggle caret tag="div" className="navbar__login-link">
+                    <span>Cart</span>
+                    <span>0</span>
+                  </DropdownToggle>
+                  <DropdownMenu right className="dropdown-cart">
+                    <p>Your cart is empty</p>
+                  </DropdownMenu>
+                </Dropdown>
               </div>
             </div>
-          </nav>
-          <div className="container">
-            <Route path="/" exact>
-                <img className="d-block mx-auto mt-5 mw-100" src="https://hackernoon.com/hn-images/1*huCrw2ybIkyg6NzbhvrAeA.jpeg" />
-            </Route>
-            <Route path="/books" exact>
-                <Title level={2}>List Books - Ant Design</Title>
-                <Row gutter={30}>
-                  {books.length > 0 &&
-                    books.map((item, index) => {
-                      return (
-                        <Col key={index} span={24} sm={12} md={6}>
-                          <Card hoverable cover={<img alt={item.title} src={item.coverUrl} />}>
-                            <Meta title={item.title} description={item.desc} />
-                            <button type="button" className="ant-btn mt-2 ant-btn-primary ant-btn-round">
-                              <span>Add to cart</span>
-                            </button>
-                          </Card>
-                        </Col>
-                      );
-                    })}
-                </Row>
-            </Route>
-            <Route path="/login" exact>
-                <LoginForm />
-            </Route>
           </div>
+        </nav>
+        <div className="container">
+          <Route path="/" exact>
+            <img className="d-block mx-auto mt-5 mw-100" src="https://hackernoon.com/hn-images/1*huCrw2ybIkyg6NzbhvrAeA.jpeg" />
+          </Route>
+          <Route path="/books" exact>
+            <Title level={2}>List Books - Ant Design</Title>
+            <Row gutter={30}>
+              {books.length > 0 &&
+                books.map((item, index) => {
+                  return (
+                    <Col key={index} span={24} sm={12} md={6}>
+                      <Card hoverable cover={<img alt={item.title} src={item.coverUrl} />}>
+                        <Meta title={item.title} description={item.desc} />
+                        <button type="button" className="ant-btn mt-2 ant-btn-primary ant-btn-round" data-id={item._id}>
+                          <span>Add to cart</span>
+                        </button>
+                      </Card>
+                    </Col>
+                  );
+                })}
+            </Row>
+          </Route>
+          <Route path="/login" exact>
+            <LoginForm />
+          </Route>
         </div>
-      </Router>
-    );
-  }
+      </div>
+    </Router>
+  );
 }
 
 export default App;
